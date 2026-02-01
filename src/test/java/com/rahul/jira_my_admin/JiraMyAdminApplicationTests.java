e75cdb0072d118bbfa90838f06d019566bafb0a1
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2026-02-01 08:34:10 +0530

    Initial Spring Boot setup for Jira connector

---
+++ src/test/java/com/rahul/jira_my_admin/JiraMyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.jira_my_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraMyAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
